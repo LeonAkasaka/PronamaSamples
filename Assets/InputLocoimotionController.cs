3d744400d50d7716c194ecbec56e8ef1246d6448
Leon Akasaka <leon@wisdomsoft.jp>
2014-10-25 18:44:19 +0900

    ユニティちゃん表示して、衝突判定できる程度にした。

---
+++ Assets/InputLocoimotionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputLocoimotionController : LocomotionController
+{
+    private void FixedUpdate()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        Locomotor.Move(v);
+        Locomotor.Rotate(h);
+
+        Locomotor.Motion.Speed = v;
+        Locomotor.Motion.Direction = h;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Locomotor.Jump();
+            StartCoroutine(Locomotor.Motion.Jump());
+        }
+    }
+}
--- Assets/PronamaChanController.cs
+++ Assets/PronamaChanController.cs
@@ -1,59 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Animator))]
-public class PronamaChanController : MonoBehaviour
+public class PronamaChanController : Locomotor
 {
-    private UnityChanLocomotions _motion;
-
     [SerializeField]
-    private float forwardSpeed = 7.0f;
+    private GameObject _targetObject = null;
+    public GameObject TargetObject { get { return _targetObject; } }
 
-    [SerializeField]
-    private float backwardSpeed = 2.0f;
-
-    [SerializeField]
-    private float rotateSpeed = 2.0f;
-
-    [SerializeField]
-    private float jumpPower = 3.0f; 
-
-	void Start ()
+    void OnCollisionEnter(Collision collision)
     {
-        var animetor = GetComponent<Animator>();
-        _motion = new UnityChanLocomotions(animetor);
-	}
-
-	void Update ()
-    {
-        var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
-
-        Move(v);
-        Rotate(h);
-
-        _motion.Speed = v;
-        _motion.Direction = h;
-
-        if (Input.GetButtonDown("Jump"))
+        if (collision.gameObject == TargetObject)
         {
-            StartCoroutine(_motion.Jump());
+            GameObject.Destroy(TargetObject);
         }
-	}
-
-    private void Move(float v)
-    {
-        var velocity = new Vector3(0, 0, v);
-        velocity = transform.TransformDirection(velocity);
-        velocity *=
-            v > 0.1 ? forwardSpeed :
-                v < -0.1 ? backwardSpeed :
-                    1;
-        transform.localPosition += velocity * Time.fixedDeltaTime;
-    }
-
-    private void Rotate(float h)
-    {
-        transform.Rotate(0, h * rotateSpeed, 0);
     }
 }
