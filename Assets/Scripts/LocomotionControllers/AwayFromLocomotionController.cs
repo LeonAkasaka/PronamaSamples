77a7fb39b9b4cf6b2d27422c3b1c81c24858e19d
Leon Akasaka <leon@wisdomsoft.jp>
2014-10-27 00:15:49 +0900

    指定のゲームオブジェクトから逃げるように移動する LocomotionController を追加。

---
+++ Assets/Scripts/LocomotionControllers/AwayFromLocomotionController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 指定のゲームオブジェクトから離れるように運動する LocomotionController。
+/// </summary>
+public class AwayFromLocomotionController : LocomotionController
+{
+    [SerializeField]
+    private GameObject _from = null;
+    public GameObject From { get { return _from; } }
+
+    [SerializeField]
+    private float _maxDistance = 10;
+    public float MaxDistance { get { return _maxDistance; } }
+
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(MotionIterator());
+    }
+
+    private IEnumerator MotionIterator()
+    {
+        yield return null;
+
+        while(true)
+        {
+            if (From == null)
+            {
+                yield return null;
+                continue;
+            }
+
+            //TODO: ↓本当は Locomotors.Rotate() で回転させるべき。
+            var v = transform.position - From.transform.position; //From ゲームオブジェクトと逆方向のベクトル
+            v.y = 0; //高さは無視する
+            transform.rotation = Quaternion.LookRotation(v.normalized);
+
+            var distance = Vector3.Distance(transform.position, From.transform.position);
+            Locomotors.Move(distance < MaxDistance ? 1.0F : 0.0F);
+
+            yield return null;
+        }
+    }
+}
