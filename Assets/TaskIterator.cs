6db6507d09cea28b47d2a476ea7b3f1cdad450f6
Leon Akasaka <leon@wisdomsoft.jp>
2014-10-29 01:30:44 +0900

    IteratorTasks 導入

---
+++ Assets/TaskIterator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using IteratorTasks;
+
+public class TaskIterator : MonoBehaviour
+{
+    private TaskScheduler _scheduler = Task.DefaultScheduler;
+	
+    private void Start()
+    {
+        _scheduler.UnhandledException += UnhandledException;
+    }
+
+    private void UnhandledException(Task obj)
+    {
+        Debug.LogError(obj.Error.ToString());
+    }
+
+	private void Update ()
+    {
+        _scheduler.Update();
+	}
+}
